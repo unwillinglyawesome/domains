cb43b3f66ff9e1ec8fd5f2f2ff5a87ece913289d
unwillinglyawesome <unwillinglyawesome@gmail.com>
2025-07-01 03:08:12 +0300

    init

---
+++ dpi_list_builder.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+domains = []
+
+src_dir = Path("dpi")
+for filepath in src_dir.iterdir():
+    if filepath.is_file():
+        with open(filepath, 'r', encoding='utf-8') as file:
+            for line in file:
+                domains.append(line.strip())
+
+domains.sort()
+
+with open('lists/zapret.lst', 'w', encoding='utf-8', newline='\n') as file:
+    for domain in domains:
+        file.write(domain + '\n')
